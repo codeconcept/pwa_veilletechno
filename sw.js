33323dc1a5df9e201931a12156a37dd5fc06eb75
codeconcept <contact@codeconcept.fr>
2017-10-29 14:46:31 +0100

    serving a custom response when user is not online

--- sw.js
+++ sw.js
@@ -7,6 +7,11 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Apllication en mode dégradé. Veuillez vous connecter</div>', headers));
+    }
+
     console.log('sw intercepte la requête suivante via fetch', evt);
     console.log('url interceptée', evt.request.url);
 });
