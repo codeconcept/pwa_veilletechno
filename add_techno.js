581e84515d64a62f1744c2e32c9f057716993924
codeconcept <contact@codeconcept.fr>
2017-10-29 13:43:19 +0100

    adding start script for json-server + fetch that makes a POST to API

--- add_techno.js
+++ add_techno.js
@@ -1,4 +1,28 @@
 console.log('hello depuis add_techno');
 
-const technonameField = document.querySelector('#technoname');
-const btnAdd = document.querySelector('#add');
+const technonameField = document.querySelector('#techno-name');
+const technoDescriptionField = document.querySelector('#techno-description');
+const technoUrlField = document.querySelector('#techno-url');
+const addTechnoForm = document.querySelector('#add-techno-form');
+
+addTechnoForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    
+    const payload = {
+        name: technonameField.value,
+        description: technoDescriptionField.value,
+        url: technoUrlField.value
+    }
+
+    fetch('http://localhost:3001/technos', { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(resp => {
+            console.log(resp);
+        })
+        .catch(err =>console.error);
+})
