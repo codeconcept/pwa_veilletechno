535efe6ccd26d67b62114be2431340f62882689a
codeconcept <contact@codeconcept.fr>
2018-01-14 14:14:52 +0100

    fixing cannot read property getKey of null

--- main.js
+++ main.js
@@ -66,8 +66,8 @@ if (navigator.serviceWorker) {
                             // TODO post to a subscription DB
                             console.log('newSubscription', newSubscription);
                             // no more keys proprety directly visible on the subscription objet. So you have to use getKey()
-                            const key = subscription.getKey('p256dh');
-                            const auth = subscription.getKey('auth');
+                            const key = newSubscription.getKey('p256dh');
+                            const auth = newSubscription.getKey('auth');
                             console.log('p256dh key', key);
                             console.log('auth key', auth);
                         })
